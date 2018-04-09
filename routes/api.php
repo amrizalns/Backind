c430fa34281551e440e51b3d4e27f5d4ea286c5b
ikhsanlaisa <ikhsanlaisa96@gmail.com>
2018-04-09 13:35:28 +0700

    new

--- routes/api.php
+++ routes/api.php
@@ -17,11 +17,14 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::group(['namespace'=> 'Api'], function(){
-  Route::get('/getUser','ApiUserController@getUser');
-  Route::get('/getDataUser', 'ApiUserController@getDataUser');
-  Route::post('/postDataUser','ApiUserController@postDataUser');
+Route::group(['namespace' => 'Api'], function () {
+    Route::post('/register', 'ApiAuthenticateController@register');
+    Route::post('/login', 'ApiAuthenticateController@login');
 
-  Route::get('/getTourism','ApiBusinessController@getTourism');
-  Route::get('/getHomestay','ApiBusinessController@getHomestay');
+    Route::get('/getUser', 'ApiUserController@getUser');
+    Route::get('/getDataUser', 'ApiUserController@getDataUser');
+    Route::post('/postDataUser', 'ApiUserController@postDataUser');
+
+    Route::get('/getTourism', 'ApiBusinessController@getTourism');
+    Route::get('/getHomestay', 'ApiBusinessController@getHomestay');
 });
